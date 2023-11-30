6fe3c213be1b4ddc9f282d6472db9137826a8f61
mwangidannn <duncanmwangikinuthia@gmail.com>
2023-11-29 16:19:58 -0800

    16 task

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def magic_calculation(g, y, z):
+    """Match bytecode provided by Holberton School."""
+    if g < f:
+        return (z)
+    if z > f:
+        return (g + f)
+    return (g*f - z)

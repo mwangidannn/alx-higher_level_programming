36901e88d38a1187cbc1d4c98c252bec102b4317
mwangidannn <duncanmwangikinuthia@gmail.com>
2023-12-04 13:07:33 -0800

    review

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-     for h in range(len(my_list)):
-        print("{:d}".format(my_list[h]))
+    for g in my_list:
+        print("{:d}".format(g))
+
+

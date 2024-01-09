82eeaf6fbf2ad4c85d380b0ac4c0b6f6ddf85366
mwangidannn <duncanmwangikinuthia@gmail.com>
2024-01-09 11:19:34 -0800

    task3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import json
+"""functions that returns json """
+
+def to_json_string(my_obj):
+    """Return the JSON representation of an object."""
+    return json.dumps(my_obj)

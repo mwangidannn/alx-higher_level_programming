4d449925524009bfd0e97ea1d1ae213432365084
mwangidannn <duncanmwangikinuthia@gmail.com>
2024-01-09 10:32:48 -0800

    updatess

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Write a function that reads a text file (UTF8) and prints it to stdout """
+"""Write a function that reads a text file (UTF8) and prints it to stdout."""
+
+
 def read_file(filename=""):
-   """ Write a function that reads a text file (UTF8) and prints it to stdout """
-    
-        with open(filename, 'r', encoding='utf-8') as f:
-            print(f.read(), end= "")
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
 

3f23d97985c588ebde4d8cf34491a643032b1f55
mwangidannn <duncanmwangikinuthia@gmail.com>
2024-01-09 13:10:55 -0800

    tasks4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import json
+
+"""write function that returns an object """
+
+def from_json_string(my_str):
+
+    """ return the python object """
+    return json.loads(my_str)

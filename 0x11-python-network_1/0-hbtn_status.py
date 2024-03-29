c74edb295eb60e9ca800b25c7eb0c48df0fb69f1
mwangidannn <duncanmwangikinuthia@gmail.com>
2024-03-28 18:20:25 -0700

    script thats fetches

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
-
+"""python scripts thats Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
-# URL to fetch
-url = 'https://alx-intranet.hbtn.io/status'
 
-try:
-    # Open the URL and read its contents
-    with urllib.request.urlopen(url) as response:
-        # Read the content of the response
-        data = response.read().decode('utf-8')
-        
-        # Display the body of the response
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
         print("Body response:")
-        print("\t- type:", type(data))
-        print("\t- content:", data)
-        
-except urllib.error.URLError as e:
-    # Handle URL errors
-    print("Error:", e)
-
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
